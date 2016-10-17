8ce12505f7d57f29e8f792f65498e4cc429f9e14
Nick Brown <quik.the.nick@gmail.com>
2016-10-17 01:03:48 +0000

    add role switching and assigning of new secret word

--- public/main.js
+++ public/main.js
@@ -1,31 +1,44 @@
 var socket = io();
 var pictionary = function() {
-    var canvas, context, drawing;
-
+    var canvas, context, drawing, currentRole, guessBox;
+    
+    var assignRole = function(role) {
+        currentRole = role.role;
+        $('#main h2 span').text(currentRole);
+        if (currentRole === 'Drawer') {
+            $('#secret').text('Secret Word: ' + role.secretWord);
+            $('#top-message').hide();
+        } else {
+            $('#secret').text('');
+            $('#top-message').show();
+        }
+        
+    };
+    
     var draw = function(position) {
         context.beginPath();
-        context.arc(position.x, position.y,
-                         6, 0, 2 * Math.PI);
+        context.arc(position.x, position.y, 6, 0, 2 * Math.PI);
         context.fill();
     };
-    var guessBox;
 
     var onKeyDown = function(event) {
         if (event.keyCode != 13) { // Enter
             return;
         }
         socket.emit('guess', guessBox.val());
-        console.log(guessBox.val());
         guessBox.val('');
     };
     
     guessBox = $('#guess input');
     guessBox.on('keydown', onKeyDown);
+    
     canvas = $('canvas');
     context = canvas[0].getContext('2d');
     canvas[0].width = canvas[0].offsetWidth;
     canvas[0].height = canvas[0].offsetHeight;
     var offset = canvas.offset();
+    
+    //Only draw on mousedown
     $(window).on('mousedown', function() {
         drawing = true;
     });
@@ -37,21 +50,21 @@ var pictionary = function() {
     canvas.on('mousemove', function(event) {
         var position = {x: event.pageX - offset.left,
                         y: event.pageY - offset.top};
-        if (drawing) {
+        if (drawing && currentRole === 'Drawer') {
             draw(position);
             socket.emit('draw', position);
         }
         
     });
     
+    socket.on('assignRole', assignRole);
     socket.on('draw', draw);
     socket.on('guess', function(guess) {
-        $('#lastGuess').text('Most recent guess: ' + guess);
+        $('#lastGuess span').text(guess);
     });
     
 };
 
 $(document).ready(function() {
     pictionary();
-    
 });
--- server.js
+++ server.js
@@ -8,14 +8,65 @@ app.use(express.static('public'));
 var server = http.Server(app);
 var io = socket_io(server);
 
+var selectSecretWord = function() {
+    var words = ["word", "letter", "number", "person", "pen", "class", "people",
+    "sound", "water", "side", "place", "man", "men", "woman", "women", "boy",
+    "girl", "year", "day", "week", "month", "name", "sentence", "line", "air",
+    "land", "home", "hand", "house", "picture", "animal", "mother", "father",
+    "brother", "sister", "world", "head", "page", "country", "question",
+    "answer", "school", "plant", "food", "sun", "state", "eye", "city", "tree",
+    "farm", "story", "sea", "night", "day", "life", "north", "south", "east",
+    "west", "child", "children", "example", "paper", "music", "river", "car",
+    "foot", "feet", "book", "science", "room", "friend", "idea", "fish",
+    "mountain", "horse", "watch", "color", "face", "wood", "list", "bird",
+    "body", "dog", "family", "song", "door", "product", "wind", "ship", "area",
+    "rock", "order", "fire", "problem", "piece", "top", "bottom", "king",
+    "space"
+    ];
+    var randomNumber = Math.floor(Math.random()*words.length);
+    return words[randomNumber];
+};
+var currentSecret = selectSecretWord();
+
 io.on('connection', function (socket) {
-    console.log('Client connected');
+    var clientCount = io.engine.clientsCount;
+
+    if (clientCount === 1) {
+        socket.emit('assignRole', {
+            role: 'Drawer',
+            secretWord: currentSecret
+        });
+    } else {
+        socket.emit('assignRole', {
+            role: 'Guesser'
+        });
+    }
+    console.log(clientCount, currentSecret, 'first');
+    
     socket.on('draw', function(position) {
         socket.broadcast.emit('draw', position);
     });
     socket.on('guess', function(guess) {
         socket.broadcast.emit('guess', guess);
+        console.log(guess.toLowerCase(), currentSecret);
+        if (guess.toLowerCase() === currentSecret) {
+            console.log('fire');
+            currentSecret = selectSecretWord();
+            socket.emit('assignRole', {
+                role: 'Drawer',
+                secretWord: currentSecret
+            });
+            socket.broadcast.emit('assignRole', {
+                role: "Guesser"
+            });
+        }
     });
+    socket.on('disconnect', function() {
+        clientCount = io.engine.clientsCount;
+        console.log('Client Disconnected', clientCount);
+    });
+    
+    
     /*console.log('Client connected');
 
     socket.once('disconnect', function() {
